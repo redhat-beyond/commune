6db24e20a0167bc8617da9e4436cef93276ff21a
Elias Assy <elias.a-98@outlook.com>
2022-11-17 17:46:28 +0200

    fixed final one

--- commune/urls.py
+++ commune/urls.py
@@ -20,8 +20,8 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',views.main_page,name='Main_Page'),
+    path('admin/', admin.site.urls) ,
+    path('',views.main_page,name='Main_Page') ,
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
