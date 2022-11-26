c6a3c9fd203e74d223cb83178262432264fb195e
Ofir Tzabari <ofirtzabari@gmail.com>
2022-11-26 10:38:39 +0200

    Add class User
    adding class User and its attributes
    migrate the class

--- commune_app/models.py
+++ commune_app/models.py
@@ -1,3 +1,11 @@
-# from django.db import models
+from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    id = models.IntegerField(unique=True, primary_key=True)
+    full_name = models.CharField(max_length=250)
+    email = models.CharField(max_length=250, unique=True)
+    phone_number = models.IntegerField()
+# remove from comment after marge and and enter to the class the next 2 lines:
+# chores = models.ForeignKey(Chore, on_delete=models.CASCADE)
+# expenses = models.ForeignKey(Expense, on_delete=models.CASCADE)
