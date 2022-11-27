4b706b8c2740a8480eb0313ffdad0593480cbe12
faezzangariya <faezzangariyacs@gmail.com>
2022-11-27 13:02:14 +0200

    Adding Expense model

--- commune_app/models.py
+++ commune_app/models.py
@@ -1,3 +1,10 @@
-# from django.db import models
+from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Expense(models.Model):
+    title = models.CharField(max_length=50)
+    budget = models.IntegerField()
+    date = models.DateTimeField(default=timezone.now)
+    assign = models.ForeignKey(User, on_delete=models.CASCADE)
+    E_id = models.IntegerField(primary_key=True)
