a0872953620bca34a843307a7f898d3333130ada
RonTuretzkyMigal <turetzkyron@gmail.com>
2022-11-27 19:56:22 +0200

    Adding model

--- commune_app/models.py
+++ commune_app/models.py
@@ -1,3 +1,12 @@
-# from django.db import models
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.postgres.fields import ArrayField
 
-# Create your models here.
+
+class Commune(Models.Model):
+    users = ArrayField(AbstractUser)
+    wallet = models.IntegerField()  #Place holder until relevant models are merged
+    chores = ArrayField(IntegerField()) #Place holder until relevant models are merged
+    expenses = ArrayField(IntegerField()) #Place holder until relevant models are merged
+    decisions = ArrayField(IntegerField()) #Place holder until relevant models are merged
+    name = models.TextField()
