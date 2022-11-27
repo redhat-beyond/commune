639b95416366a466b60b48a6f0ed199d7c6794e0
Ofir Tzabari <ofirtzabari@gmail.com>
2022-11-27 18:34:15 +0200

    change to abstructuser
    change from User(models.Model) to User(AbstractUser)
    add the necessary library

--- commune_app/models.py
+++ commune_app/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 
-class User(models.Model):
+class User(AbstractUser):
     id = models.IntegerField(unique=True, primary_key=True)
     first_name = models.CharField(max_length=250)
     last_name = models.CharField(max_length=250)
