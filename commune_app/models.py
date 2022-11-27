88987a557bce26ebb0084d07f3ae0fa819933f22
Ofir Tzabari <ofirtzabari@gmail.com>
2022-11-27 08:39:09 +0200

    Change attributes on User model
    separate "full_name" to "first_name" and "last_name"
    change the email from CharField to EmailField

--- commune_app/models.py
+++ commune_app/models.py
@@ -3,8 +3,9 @@ from django.db import models
 
 class User(models.Model):
     id = models.IntegerField(unique=True, primary_key=True)
-    full_name = models.CharField(max_length=250)
-    email = models.CharField(max_length=250, unique=True)
+    first_name = models.CharField(max_length=250)
+    last_name = models.CharField(max_length=250)
+    email = models.EmailField(max_length=250, unique=True)
     phone_number = models.IntegerField()
 # remove from comment after marge and and enter to the class the next 2 lines:
 # chores = models.ForeignKey(Chore, on_delete=models.CASCADE)
