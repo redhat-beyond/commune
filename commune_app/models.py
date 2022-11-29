79b9075f3c660ff95f05e9ef0d1ce2a80b9bb461
Ofir Tzabari <ofirtzabari@gmail.com>
2022-11-29 23:23:12 +0200

    Edit User model
    1. import PhoneNumberField library and use in phone_number attributes
    2. fix the max length of first and last name to 50 char
    3. create constructor for User model
    4. initial and migrate according to the new changes the migrations

--- commune_app/models.py
+++ commune_app/models.py
@@ -1,13 +1,32 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 class User(AbstractUser):
     id = models.IntegerField(unique=True, primary_key=True)
-    first_name = models.CharField(max_length=250)
-    last_name = models.CharField(max_length=250)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
     email = models.EmailField(max_length=250, unique=True)
-    phone_number = models.IntegerField()
+    phone_number = PhoneNumberField()
 # remove from comment after marge and and enter to the class the next 2 lines:
 # chores = models.ForeignKey(Chore, on_delete=models.CASCADE)
 # expenses = models.ForeignKey(Expense, on_delete=models.CASCADE)
+
+    @staticmethod
+    def create_user(id, first_name, last_name, email, phone_number):
+        """"
+        Method creates a new user
+        param id: user's id
+        param first_name: user's first name
+        param last_name: user's last name
+        param email: user's last email
+        param phone_number: user's phone number
+        """
+        try:
+            user = User.objects.get(id=id)
+            return user
+        except User.DoesNotExist:
+            user = User(id=id, first_name=first_name, last_name=last_name, email=email, phone_number=phone_number)
+            user.save()
+            return user
