1d91df112bbd5d017233e1b568e9c05568f4e110
faezzangariya <faezzangariyacs@gmail.com>
2022-11-29 03:33:08 +0200

    more flake8 line too long fixes

--- commune_app/migrations/0001_initial.py
+++ commune_app/migrations/0001_initial.py
@@ -24,17 +24,20 @@ class Migration(migrations.Migration):
                 ('password', models.CharField(max_length=128, verbose_name='password')),
                 ('last_login', models.DateTimeField(blank=True, null=True, verbose_name='last login')),
                 ('is_superuser', models.BooleanField(
-                    default=False, help_text='Designates that this user has all permissions without explicitly assigning them.',
+                    default=False,
+                    help_text='Designates that this user has all permissions without explicitly assigning them.',
                     verbose_name='superuser status')),
                 ('username', models.CharField(
                     error_messages={'unique': 'A user with that username already exists.'},
-                    help_text='Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.', max_length=150, unique=True,
+                    help_text='Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.',
+                    max_length=150, unique=True,
                     validators=[django.contrib.auth.validators.UnicodeUsernameValidator()], verbose_name='username')),
                 ('is_staff', models.BooleanField(
-                    default=False, help_text='Designates whether the user can log into this admin site.', verbose_name='staff status')),
+                    default=False, help_text='Designates whether the user can log into this admin site.',
+                    verbose_name='staff status')),
                 ('is_active', models.BooleanField(
                     default=True,
-                    help_text='Designates whether this user should be treated as active. Unselect this instead of deleting accounts.',
+                    help_text='Designates whether this user should be treated as active.',
                     verbose_name='active')),
                 ('date_joined', models.DateTimeField(default=django.utils.timezone.now, verbose_name='date joined')),
                 ('first_name', models.CharField(max_length=250)),
@@ -42,7 +45,8 @@ class Migration(migrations.Migration):
                 ('email', models.EmailField(max_length=250, unique=True)),
                 ('phone_number', models.IntegerField()),
                 ('groups', models.ManyToManyField(
-                    blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.',
+                    blank=True,
+                    help_text='The groups this user belongs to.',
                     related_name='user_set', related_query_name='user', to='auth.group', verbose_name='groups')),
                 ('user_permissions', models.ManyToManyField(
                     blank=True, help_text='Specific permissions for this user.',
