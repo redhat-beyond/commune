a9a9a6a96b64846d7b833cda0290b45fe57dc98d
faezzangariya <faezzangariyacs@gmail.com>
2022-11-30 03:47:11 +0200

    fixing pytest failed run

--- commune_app/models.py
+++ commune_app/models.py
@@ -1,20 +1,34 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import AbstractUser
-from django.core.exceptions import ObjectDoesNotExist
+from django.core.validators import RegexValidator
 
 
 class User(AbstractUser):
-    first_name = models.CharField(max_length=250)
-    last_name = models.CharField(max_length=250)
+    id = models.IntegerField(unique=True, primary_key=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
     email = models.EmailField(max_length=250, unique=True)
-    phone_number = models.IntegerField()
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+    phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True)
 
     @staticmethod
-    def create_user(first_name, last_name, email, phone_number):
-        user = Expense(first_name=first_name, last_name=last_name, email=email, phone_number=phone_number)
-        user.save()
-        return user
+    def create_user(id, first_name, last_name, email, phone_number):
+        """"
+        Method creates a new user
+        param id: user's id
+        param first_name: user's first name
+        param last_name: user's last name
+        param email: user's last email
+        param phone_number: user's phone number
+        """
+        try:
+            user = User.objects.get(id=id)
+            return user
+        except User.DoesNotExist:
+            user = User(id=id, first_name=first_name, last_name=last_name, email=email, phone_number=phone_number)
+            user.save()
+            return user
 
 
 class Expense(models.Model):
@@ -25,15 +39,26 @@ class Expense(models.Model):
 
     @staticmethod
     def create_expense(title, budget, date, assign):
-        expense = Expense(title=title, budget=budget, date=date, assign=assign)
-        expense.save()
-        return expense
+        """"
+        Method creates a new expense
+        param title: expense's title
+        param budget: expense's budget
+        param date: expense's date
+        param assign: expense's assign
+        """
+        try:
+            expense = Expense.objects.get(title=title)
+            return expense
+        except Expense.DoesNotExist:
+            expense = Expense(title=title, budget=budget, date=date, assign=assign)
+            expense.save()
+            return expense
 
     @staticmethod
     def get_expense_by_title(title):
         try:
             expense = Expense.objects.filter(title=title).order_by('date').first()
-        except ObjectDoesNotExist:
+        except Expense.DoesNotExist:
             return None
         return expense
 
@@ -41,6 +66,6 @@ class Expense(models.Model):
     def get_AllExpenses_by_user(title):
         try:
             expenses = Expense.objects.all().filter(title=title).order_by('date').first()
-        except ObjectDoesNotExist:
+        except Expense.DoesNotExist:
             return None
         return expenses
