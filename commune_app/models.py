08879061b2e1fbfa652a856a3377819d2d26642e
faezzangariya <faezzangariyacs@gmail.com>
2022-11-30 02:50:23 +0200

    making small changes to commune_app/models.py

--- commune_app/models.py
+++ commune_app/models.py
@@ -1,20 +1,33 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import AbstractUser
-from django.core.exceptions import ObjectDoesNotExist
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 class User(AbstractUser):
-    first_name = models.CharField(max_length=250)
-    last_name = models.CharField(max_length=250)
+    id = models.IntegerField(unique=True, primary_key=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
     email = models.EmailField(max_length=250, unique=True)
-    phone_number = models.IntegerField()
+    phone_number = PhoneNumberField()
 
     @staticmethod
-    def create_user(first_name, last_name, email, phone_number):
-        user = Expense(first_name=first_name, last_name=last_name, email=email, phone_number=phone_number)
-        user.save()
-        return user
+    def create_user(id, first_name, last_name, email, phone_number):
+        """"
+        Method creates a new user
+        param id: user's id
+        param first_name: user's first name
+        param last_name: user's last name
+        param email: user's last email
+        param phone_number: user's phone number
+        """
+        try:
+            user = User.objects.get(id=id)
+            return user
+        except User.DoesNotExist:
+            user = User(id=id, first_name=first_name, last_name=last_name, email=email, phone_number=phone_number)
+            user.save()
+            return user
 
 
 class Expense(models.Model):
@@ -25,15 +38,26 @@ class Expense(models.Model):
 
     @staticmethod
     def create_expense(title, budget, date, assign):
-        expense = Expense(title=title, budget=budget, date=date, assign=assign)
-        expense.save()
-        return expense
+        """"
+        Method creates a new expense
+        param title: expense's title
+        param budget: expense's budget
+        param date: expense's date
+        param assign: expense's assign
+        """
+        try:
+            expense = Expense.objects.get(title=title)
+            return expense
+        except Expense.DoesNotExist:
+            expense = Expense(title=title, budget=budget, date=date, assign=assign)
+            expense.save()
+            return expense
 
     @staticmethod
     def get_expense_by_title(title):
         try:
             expense = Expense.objects.filter(title=title).order_by('date').first()
-        except ObjectDoesNotExist:
+        except Expense.DoesNotExist:
             return None
         return expense
 
@@ -41,6 +65,6 @@ class Expense(models.Model):
     def get_AllExpenses_by_user(title):
         try:
             expenses = Expense.objects.all().filter(title=title).order_by('date').first()
-        except ObjectDoesNotExist:
+        except Expense.DoesNotExist:
             return None
         return expenses
