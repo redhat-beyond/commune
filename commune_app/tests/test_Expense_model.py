7b054571d3a09fb6165c1a1fb5527e48fa198853
faezzangariya <faezzangariyacs@gmail.com>
2022-12-01 16:02:06 +0200

    added suggested changes to test files
    1)added some fixtures
    2)removed tests related to the create object methods that were not
      neccessary
    
    Signed-off-by: faezzangariya <faezzangariyacs@gmail.com>

--- commune_app/tests/test_Expense_model.py
+++ commune_app/tests/test_Expense_model.py
@@ -1,30 +1,64 @@
 from ..models import Expense, User
 import pytest
 
+USER1_ID = 1115799993
+USER1_FIRST_NAME = "Faez"
+USER1_LAST_NAME = "Zangariya"
+USER1_EMAIL = "faezzangariyacs@gmail.com"
+USER1_PHONE_NUMBER = "+972523324193"
+
+EXPENSE1_TITLE = "X"
+EXPENSE1_BUDGET = 100
+EXPENSE1_DATE = "2011-09-01T13:20:30+03:00"
+
+EXPENSE2_TITLE = "Y"
+EXPENSE2_BUDGET = 200
+EXPENSE2_DATE = "2011-09-01T13:20:30+03:00"
+
+EXPENSE3_TITLE = "Z"
+EXPENSE3_BUDGET = 300
+EXPENSE3_DATE = "2011-09-01T13:20:30+03:00"
+
+
+@pytest.fixture
+def user1():
+    return User(
+        id=USER1_ID,
+        first_name=USER1_FIRST_NAME,
+        last_name=USER1_LAST_NAME,
+        email=USER1_EMAIL,
+        phone_number=USER1_PHONE_NUMBER
+    )
+
+
+@pytest.fixture
+def expense1(user1):
+    return Expense(title=EXPENSE1_TITLE, budget=EXPENSE1_BUDGET, date=EXPENSE1_DATE, assign=user1)
+
+
+@pytest.fixture
+def expense2(user1):
+    return Expense(title=EXPENSE2_TITLE, budget=EXPENSE2_BUDGET, date=EXPENSE2_DATE, assign=user1)
+
+
+@pytest.fixture
+def expense3(user1):
+    return Expense(title=EXPENSE3_TITLE, budget=EXPENSE3_BUDGET, date=EXPENSE3_DATE, assign=user1)
+
 
 class TestExpenseModel:
-    @pytest.mark.django_db
-    def test_new_expense(self):
-        expense = Expense(title="buying some food", budget="150", date="2011-09-01T13:20:30+03:00", assign=User(first_name="Faez", last_name="Zangariya", email="faezzangariyacs@gmail.com", phone_number="+972523324193"))
-        assert expense.title == "buying some food"
-        assert expense.budget == "150"
-        assert expense.date == "2011-09-01T13:20:30+03:00"
-        assert expense.assign.first_name == "Faez"
+    def test_new_expense(self, expense1):
+        assert expense1.title == "X"
+        assert expense1.budget == 100
+        assert expense1.date == "2011-09-01T13:20:30+03:00"
+        assert expense1.assign.first_name == "Faez"
 
     @pytest.mark.django_db
-    def test_create_expense(self):
-        expense = Expense.create_expense(title="Laundry", budget="200", date="2011-09-01T13:20:30+03:00", assign=User.create_user(first_name="Ron", last_name="Tur", email="rontur@gmail.com", phone_number="+972523324193"))
-        assert expense.title == "Laundry"
-        assert expense.budget == "200"
-        assert expense.date == "2011-09-01T13:20:30+03:00"
-        assert expense.assign.first_name == "Ron"
-
-    @pytest.mark.django_db
-    def test_get_expense_by_title(self):
-        currU = User.create_user(first_name="ofir", last_name="tza", email="ofirtza@gmail.com", phone_number="+972523324193")
-        expense1 = Expense.create_expense(title="X", budget="100", date="2011-09-01T13:20:30+03:00", assign=currU)
-        expense2 = Expense.create_expense(title="Y", budget="200", date="2011-09-01T13:20:30+03:00", assign=currU)
-        expense3 = Expense.create_expense(title="Z", budget="300", date="2011-09-01T13:20:30+03:00", assign=currU)
+    def test_get_expense_by_title(self, user1, expense1, expense2, expense3):
+        user1.save()
+        expense1.save()
+        expense2.save()
+        expense3.save()
         assert Expense.get_expense_by_title("X") == expense1
         assert Expense.get_expense_by_title("Y") == expense2
         assert Expense.get_expense_by_title("Z") == expense3
