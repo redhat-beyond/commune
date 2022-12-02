b0342c17020e7297acd03d7d9924078e41caa471
faezzangariya <faezzangariyacs@gmail.com>
2022-12-02 23:03:56 +0200

    updating test file after addition of a new field

--- commune_app/tests/test_Expense_model.py
+++ commune_app/tests/test_Expense_model.py
@@ -7,14 +7,17 @@ USER1_LAST_NAME = "Zangariya"
 USER1_EMAIL = "faezzangariyacs@gmail.com"
 USER1_PHONE_NUMBER = "+972523324193"
 
+EXPENSE1_ID = 1
 EXPENSE1_TITLE = "X"
 EXPENSE1_BUDGET = 100
 EXPENSE1_DATE = "2011-09-01T13:20:30+03:00"
 
+EXPENSE2_ID = 2
 EXPENSE2_TITLE = "Y"
 EXPENSE2_BUDGET = 200
 EXPENSE2_DATE = "2011-09-01T13:20:30+03:00"
 
+EXPENSE3_ID = 3
 EXPENSE3_TITLE = "Z"
 EXPENSE3_BUDGET = 300
 EXPENSE3_DATE = "2011-09-01T13:20:30+03:00"
@@ -33,35 +36,36 @@ def user1():
 
 @pytest.fixture
 def expense1(user1):
-    return Expense(title=EXPENSE1_TITLE, budget=EXPENSE1_BUDGET, date=EXPENSE1_DATE, assign=user1)
+    return Expense(id=EXPENSE1_ID, title=EXPENSE1_TITLE, budget=EXPENSE1_BUDGET, date=EXPENSE1_DATE, assign=user1)
 
 
 @pytest.fixture
 def expense2(user1):
-    return Expense(title=EXPENSE2_TITLE, budget=EXPENSE2_BUDGET, date=EXPENSE2_DATE, assign=user1)
+    return Expense(id=EXPENSE2_ID, title=EXPENSE2_TITLE, budget=EXPENSE2_BUDGET, date=EXPENSE2_DATE, assign=user1)
 
 
 @pytest.fixture
 def expense3(user1):
-    return Expense(title=EXPENSE3_TITLE, budget=EXPENSE3_BUDGET, date=EXPENSE3_DATE, assign=user1)
+    return Expense(id=EXPENSE3_ID, title=EXPENSE3_TITLE, budget=EXPENSE3_BUDGET, date=EXPENSE3_DATE, assign=user1)
 
 
 class TestExpenseModel:
     def test_new_expense(self, expense1):
+        assert expense1.id == 1
         assert expense1.title == "X"
         assert expense1.budget == 100
         assert expense1.date == "2011-09-01T13:20:30+03:00"
         assert expense1.assign.first_name == "Faez"
 
     @pytest.mark.django_db
-    def test_get_expense_by_title(self, user1, expense1, expense2, expense3):
+    def test_get_expense_by_pk(self, user1, expense1, expense2, expense3):
         user1.save()
         expense1.save()
         expense2.save()
         expense3.save()
-        assert Expense.get_expense_by_title("X") == expense1
-        assert Expense.get_expense_by_title("Y") == expense2
-        assert Expense.get_expense_by_title("Z") == expense3
-        assert Expense.get_expense_by_title("X").budget == 100
-        assert Expense.get_expense_by_title("Y").budget == 200
-        assert Expense.get_expense_by_title("Z").budget == 300
+        assert Expense.get_expense_by_pk(1).title == "X"
+        assert Expense.get_expense_by_pk(2).title == "Y"
+        assert Expense.get_expense_by_pk(3).title == "Z"
+        assert Expense.get_expense_by_pk(1).budget == 100
+        assert Expense.get_expense_by_pk(2).budget == 200
+        assert Expense.get_expense_by_pk(3).budget == 300
