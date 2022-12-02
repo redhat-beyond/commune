e9a26b50ff1c4c69c35d58fa97d1193ebe7e2815
Ofir Tzabari <ofirtzabari@gmail.com>
2022-12-02 15:21:55 +0200

    add configuration
    install and add the phonenumber package to pipfile
    also remove User constructor

--- commune_app/models.py
+++ commune_app/models.py
@@ -12,21 +12,3 @@ class User(AbstractUser):
     last_name = models.CharField(max_length=50)
     email = models.EmailField(max_length=250, unique=True)
     phone_number = models.CharField(validators=[phone_regax], max_length=17, blank=True)
-
-    @staticmethod
-    def create_user(id, first_name, last_name, email, phone_number):
-        """"
-        Method creates a new user
-        param id: user's id
-        param first_name: user's first name
-        param last_name: user's last name
-        param email: user's last email
-        param phone_number: user's phone number
-        """
-        try:
-            user = User.objects.get(id=id)
-            return user
-        except User.DoesNotExist:
-            user = User(id=id, first_name=first_name, last_name=last_name, email=email, phone_number=phone_number)
-            user.save()
-            return user
