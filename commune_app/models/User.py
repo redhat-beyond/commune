ce80e337962232c84437de097a57080e86cf79ad
faezzangariya <faezzangariyacs@gmail.com>
2022-12-27 05:10:19 +0200

    Adding Vote model
    adding the vote model without tests yet ,in order to add the tests i
    need a boolean variable called passed added to the chore model and a
    method that returns a chore by id, also i need a method that returns the
    number of members in the commune
    
    Signed-off-by: faezzangariya <faezzangariyacs@gmail.com>

---
+++ commune_app/models/User.py
@@ -0,0 +1,43 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import validate_email
+from django.core.exceptions import ValidationError
+
+
+def validate_email_addr(email):
+    try:
+        validate_email(email)
+    except ValidationError:
+        raise ValidationError('Email is not valid')
+
+
+class User(AbstractUser):
+    '''
+    we use the field of Abstructuser(the required):
+    id
+    username
+    password
+    first_name
+    last_name
+    email
+    we add:
+    validator for email
+    '''
+    email = models.EmailField(unique=True, validators=[validate_email_addr])
+
+    def clean(self) -> None:
+        validate_email(self.email)
+        return super().clean()
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        return super().save(*args, **kwargs)
+
+    def vote_on_decision(decision_id):
+        pass
+
+    def add_decision(args):
+        pass
+
+    def execute_chore():
+        pass
