79f98985af0e4de23aaa93d56455895c85301734
faezzangariya <faezzangariyacs@gmail.com>
2022-12-29 16:54:51 +0200

    registering User model to admin

--- commune_app/admin.py
+++ commune_app/admin.py
@@ -1,3 +1,5 @@
-# from django.contrib import admin
+from django.contrib import admin
+from .all_models.users import User
 
-# Register your models here.
+# Register your models here. # Register your models here.
+admin.site.register(User)
