9f0242149706f8ce69c85c53a3d2bb7094685e15
Elias Assy <elias.a-98@outlook.com>
2023-01-02 12:48:49 +0200

    migrate didnt work

--- commune_app/models.py
+++ commune_app/models.py
@@ -1,1 +1,2 @@
 from commune_app.all_models.users import User  # noqa:F401
+from commune_app.all_models.chores import Chore  # noqa:F401
--- commune_app/views.py
+++ commune_app/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import Chore
 
 
 def main_page(request):
     return render(request, 'commune_app/index.html')
+
+
+def chores(request):
+    chores = Chore.objects.all()
+    context = {'chores': chores}
+    return render(request, 'commune_app/chores.html', context)
