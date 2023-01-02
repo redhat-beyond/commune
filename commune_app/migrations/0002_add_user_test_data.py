8a15faf39fefd817e01248a92f80ea3d752cb952
Ofir Tzabari <ofirtzabari@gmail.com>
2023-01-02 19:18:53 +0200

    fix change request

--- commune_app/migrations/0002_add_user_test_data.py
+++ commune_app/migrations/0002_add_user_test_data.py
@@ -11,15 +11,14 @@ class Migration(migrations.Migration):
         from ..all_models.users import User
 
         users_test_data = [
-            ('testUser123', 'testUser1@mta.ac.il', 'PasswordU$er123', '123'),
-            ('testUser234', 'testUser2@mta.ac.il', 'PasswordU$er456', '234'),
-            ('testUser345', 'testUser3@mta.ac.il', 'PasswordU$er789', '345')
+            ('testUser123', 'testUser1@telhai.ac.il', 'PasswordU$er123', '123'),
+            ('testUser234', 'testUser2@telhai.ac.il', 'PasswordU$er456', '234'),
+            ('testUser345', 'testUser3@telhai.ac.il', 'PasswordU$er789', '345')
         ]
 
         with transaction.atomic():
             for user_name, user_email, user_password, user_id in users_test_data:
                 user = User(username=user_name, email=user_email, password=user_password, id=user_id)
-                user.set_password(user_password)
                 user.save()
 
     operations = [
