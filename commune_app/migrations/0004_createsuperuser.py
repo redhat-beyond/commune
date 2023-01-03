26bf47052b6f25a073ea58c3819c7821731e2b76
Ofir Tzabari <ofirtzabari@gmail.com>
2023-01-03 17:27:46 +0200

    push again
    Removal of "is_active" which is not necessary

--- commune_app/migrations/0004_createsuperuser.py
+++ commune_app/migrations/0004_createsuperuser.py
@@ -6,8 +6,8 @@ from commune_app.all_models.users import User
 def create_superuser(apps, schema_editor):
     with transaction.atomic():
         superuser = User(
-            is_active=True, is_superuser=True, is_staff=True, username='admin', email='admin@gmail.com'
-            )
+        is_superuser=True, is_staff=True, username='admin', email='admin@gmail.com'
+        )
         superuser.set_password('adminPassword')
         superuser.save()
 
