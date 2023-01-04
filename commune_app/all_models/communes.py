8901605e642fe144a4b9d070049773f5b149ff64
Ofir Tzabari <ofirtzabari@gmail.com>
2023-01-04 14:36:59 +0200

    fix flake8

--- commune_app/all_models/communes.py
+++ commune_app/all_models/communes.py
@@ -12,14 +12,14 @@ class Commune(models.Model):
     users = models.ManyToManyField(User)
     votes = models.ForeignKey(Vote, on_delete=models.CASCADE)
     chores = models.ForeignKey(Chore, on_delete=models.CASCADE)
-    
+
     wallet = models.IntegerField()
     name = models.TextField()
 
-    @staticmethod
-    def fetch_commune_stats(id):
-        try:
-            commune = commune.objects.filter(id=id)
-        except Commune.DoesNotExist:
-            return None
-        return commune
+# @staticmethod
+# def fetch_commune_stats(id):
+#     try:
+#         commune = commune.objects.filter(id=id)  //dont know what try in function
+#     except Commune.DoesNotExist:
+#         return None
+#     return commune
