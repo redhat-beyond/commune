11e3b98d63e92bdad0510a25d1f154dd61b150be
Ofir Tzabari <ofirtzabari@gmail.com>
2023-01-12 16:37:15 +0200

    implement user functions
    edit and implement user functions
    problem with circle import
    fix #47

--- commune_app/all_models/users.py
+++ commune_app/all_models/users.py
@@ -4,6 +4,7 @@ from django.core.validators import validate_email
 from django.core.exceptions import ValidationError
 from commune_app.all_models.communes import Commune
 from commune_app.all_models.votes import Vote
+from commune_app.all_models.chores import Chore
 
 
 def validate_email_addr(email):
@@ -46,6 +47,11 @@ class User(AbstractUser):
     def leave_commune(self):
         if (self.commune_id is not None):
             self.commune_id = None
+
     def vote(self, chore_id, decision):
         Vote.create_new_vote(voting_user=self.id, voted_chore=chore_id, vote_bool=decision)
 
+    def execute_chore(self, chore_id):
+        chore = Chore.objects.filter(id=chore_id)[0]
+        if (chore.passed is True and chore.assign_to == self.id):
+            chore.completed = True
