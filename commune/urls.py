4fbb9c9499400d072aad9ab09e98824a37148aca
RonTuretzkyMigal <turetzkyron@gmail.com>
2023-01-17 14:51:40 +0200

    adding logout url

--- commune/urls.py
+++ commune/urls.py
@@ -29,7 +29,7 @@ urlpatterns = [
     path('login/', views.user_login, name="login"),
     path('signup/', views.user_signup, name="signup"),
     path('accounts/login/', auth_views.LoginView.as_view(template_name='registration/login.html')),
-
+    path('accounts/logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout')
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
