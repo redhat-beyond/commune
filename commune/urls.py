eeb4194e34327e1d55d18098b6db8a531202a1c8
RonTuretzkyMigal <turetzkyron@gmail.com>
2023-01-19 21:58:58 +0200

    adding commune creation url

--- commune/urls.py
+++ commune/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     path('commune/', views.commune, name="commune"),
     path('login/', views.user_login, name="login"),
     path('signup/', views.user_signup, name="signup"),
-    path('logout/', views.user_logout, name='logout')
+    path('logout/', views.user_logout, name='logout'),
     path('create_commune/', views.create_commune, name="create_commune"),
 ]
 
