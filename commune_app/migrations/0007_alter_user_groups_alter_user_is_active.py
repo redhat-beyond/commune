6adec7666e195e82a543022d43156a17064af6f3
RonTuretzkyMigal <turetzkyron@gmail.com>
2023-01-19 22:56:38 +0200

    adding more migrations

--- commune_app/migrations/0007_alter_user_groups_alter_user_is_active.py
+++ commune_app/migrations/0007_alter_user_groups_alter_user_is_active.py
@@ -14,11 +14,15 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='user',
             name='groups',
-            field=models.ManyToManyField(blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', related_name='user_set', related_query_name='user', to='auth.group', verbose_name='groups'),
+            field=models.ManyToManyField(blank=True, help_text='The groups this user\
+             belongs to. A user will get all permissions granted to each of their \
+             groups.', related_name='user_set', related_query_name='user', to='auth.group', verbose_name='groups'),
         ),
         migrations.AlterField(
             model_name='user',
             name='is_active',
-            field=models.BooleanField(default=True, help_text='Designates whether this user should be treated as active. Unselect this instead of deleting accounts.', verbose_name='active'),
+            field=models.BooleanField(default=True, help_text='Designates whether\
+             this user should be treated as active. Unselect this instead of\
+              deleting accounts.', verbose_name='active'),
         ),
     ]
