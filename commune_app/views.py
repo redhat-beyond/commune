45eae2d5476e0fc71ba10fa0a4e8c6a596a3c6fe
RonTuretzkyMigal <turetzkyron@gmail.com>
2023-01-19 12:53:29 +0200

    updating login

--- commune_app/views.py
+++ commune_app/views.py
@@ -5,15 +5,18 @@ from .models import Chore, Commune
 
 
 def main_page(request):
-    communes = Commune.objects.all()
-    context = {'communes': communes}
-    return render(request, 'commune_app/index.html', context)
+    # communes = Commune.objects.all()
+    # context = {'communes': communes}
+    # return redirect('commune_app/index.html')
+    return render(request, 'commune_app/index.html')
 
 
 def commune(request):
-    commune_id = request.user.commune_id
-    chores = Chore.objects.filter(commune_id=commune_id,passed=False)
-    context = {'chores': chores}
+    users = get_user_model().objects.all()
+    context = {'users': users}
+#     commune_id = request.user.commune_id
+#     chores = Chore.objects.filter(commune_id=commune_id,passed=False)
+#     context = {'chores': chores}
     return render(request, 'commune_app/commune.html', context)
     # active_chores = Chore.objects.filter(commune_id=commune_id,completed=False,passed=True)
     # commune = Commune.objects.filter(id=commune_id).first()
@@ -34,7 +37,7 @@ def user_signup(request):
         user_obj.save()
         user_auth = authenticate(username=email, password=password)
         login(request, user_auth)
-        return redirect('Main_Page')
+        return redirect('main_page')
     else:
         return render(request, 'commune_app/signup.html')
 
@@ -43,15 +46,15 @@ def user_login(request):
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
-        user = authenticate(username=username, password=password)
+        user = authenticate(request,username=username, password=password)
         if user is not None:
             login(request, user)
+            return render(request, 'commune')
         else:
-            return redirect('Main_Page')
+            return redirect('logout')
     else:
-        return render(request, 'commune_app/login.html')
-
+        return render(request, 'registration/login.html')
 
 def user_logout(request):
     logout(request)
-    return redirect('Main_Page')
+    return redirect('main_page')
