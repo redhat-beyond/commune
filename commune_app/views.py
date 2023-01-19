53d284526b0f51ba6b9ed89225fb1e314c74add9
RonTuretzkyMigal <turetzkyron@gmail.com>
2023-01-19 21:55:42 +0200

    adding creating commune method, fixing up up commune fetching method

--- commune_app/views.py
+++ commune_app/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import get_user_model
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from commune_app.models import Chore, Commune, Vote
+from django.contrib.auth import get_user_model
+from django.apps import apps
+
+Chore = apps.get_model('commune_app', 'Chore')
+Commune = apps.get_model('commune_app', 'Commune')
 
 
 def main_page(request):
@@ -23,6 +26,21 @@ def has_voted(user_id, chore_id):
 
 
 def commune(request):
+    User = get_user_model()
+    user = User.objects.get(id=request.user.id)
+    commune_id = user.commune_id.id
+    if commune_id is not None:
+        active_chores = Chore.objects.filter(commune_id=commune_id, completed=False, passed=True)
+        chores_to_vote_on = Chore.objects.filter(commune_id=commune_id, completed=False, passed=False)
+        commune = Commune.objects.filter(id=commune_id).first()
+        commune_name = commune.name
+        wallet = commune.wallet
+        description = commune.description
+        context = {'active_chores': active_chores, 'commune_name': commune_name, 'chores_to_vote_on': chores_to_vote_on,
+                   'wallet': wallet, 'description': description}
+        return render(request, 'commune_app/commune.html', context)
+    else:
+        return redirect('main_page')
     commune_id = request.user.commune_id.id
     # chores = Chore.objects.filter(commune_id=commune_id, passed=False)
     active_chores = Chore.objects.filter(commune_id=commune_id, completed=False, passed=True)
@@ -109,3 +127,19 @@ def vote(request):
 def user_logout(request):
     logout(request)
     return redirect('main_page')
+
+
+def create_commune(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        description = request.POST['description']
+        wallet = request.POST['wallet']
+        new_commune = Commune.create_commune(name=name, description=description, wallet=wallet)
+        new_commune.save()
+        user_id = request.user.id
+        User = get_user_model()
+        user = User.objects.get(id=user_id)
+        user.join_commune(new_commune.id)
+        return redirect('main_page')
+    else:
+        return render(request, 'commune_app/create_commune.html')
