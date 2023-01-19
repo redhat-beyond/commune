bba6ce5c6af16b53a9251a58906d3338367967ad
RonTuretzkyMigal <turetzkyron@gmail.com>
2023-01-19 19:59:48 +0200

    adding path for view

--- commune/urls.py
+++ commune/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
     path('login/', views.user_login, name="login"),
     path('signup/', views.user_signup, name="signup"),
     path('logout/', views.user_logout, name='logout')
+    path('create_commune/', views.create_commune, name="create_commune"),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
