82d0f395f33791f9d0af8fe7035c683146572f8b
EliasAndry <116427017+EliasAndry@users.noreply.github.com>
2023-01-21 21:57:15 +0200

    Commune page (#93)
    * Update commune_app/all_models/communes.py
    
    Co-authored-by: Kobi Hakimi <kobihk@users.noreply.github.com>
    
    * fixed htmls
    
    * fixed erros
    
    * adding vote url
    
    * adding vote function, updating commune view to include more details
    
    * adding voting & vote counting
    
    * adding migration
    
    * fixed flake8
    
    fixed flake8 error
    
    Co-authored-by: RonTuretzky <74178515+RonTuretzky@users.noreply.github.com>
    Co-authored-by: Kobi Hakimi <kobihk@users.noreply.github.com>
    Co-authored-by: RonTuretzkyMigal <turetzkyron@gmail.com>

--- commune/urls.py
+++ commune/urls.py
@@ -27,7 +27,8 @@ urlpatterns = [
     path('chore/', views.chore, name='chore'),
     path('login/', views.user_login, name="login"),
     path('signup/', views.user_signup, name="signup"),
-    path('logout/', views.user_logout, name='logout')
+    path('logout/', views.user_logout, name='logout'),
+    path('vote/', views.vote, name='vote')
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- commune_app/views.py
+++ commune_app/views.py
@@ -2,16 +2,44 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import get_user_model
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from commune_app.all_models.chores import Chore
+from commune_app.models import Chore, Commune, Vote
 
 
 def main_page(request):
     return render(request, 'commune_app/index.html')
 
 
+def get_yes_votes_for_chore(chore_id):
+    return len(Vote.objects.filter(chore_id=chore_id, approve=True))
+
+
+def get_no_votes_for_chore(chore_id):
+    return len(Vote.objects.filter(chore_id=chore_id, approve=False
+                                   ))
+
+
+def has_voted(user_id, chore_id):
+    return len(Vote.objects.filter(user_id=user_id, chore_id=chore_id)) > 0
+
+
 def commune(request):
-    users = get_user_model().objects.all()
-    context = {'users': users}
+    commune_id = request.user.commune_id.id
+    # chores = Chore.objects.filter(commune_id=commune_id, passed=False)
+    active_chores = Chore.objects.filter(commune_id=commune_id, completed=False, passed=True)
+    chores_to_vote_on = Chore.objects.filter(commune_id=commune_id, completed=False, passed=False)
+    chores_to_vote_on = [(chore, get_yes_votes_for_chore(chore.id), get_no_votes_for_chore(chore.id),
+                          has_voted(request.user.id, chore.id), chore.id) for chore in chores_to_vote_on]
+    commune = Commune.objects.filter(id=commune_id).first()
+    commune_name = commune.name
+    wallet = commune.wallet
+    description = commune.description
+    context = {
+        'active_chores': active_chores,
+        'commune_name': commune_name,
+        'chores_to_vote_on': chores_to_vote_on,
+        'wallet': wallet,
+        'description': description
+    }
     return render(request, 'commune_app/commune.html', context)
 
 
@@ -59,6 +87,21 @@ def user_login(request):
             return redirect('login')
     else:
         return render(request, 'commune_app/login.html')
+
+
+def vote(request):
+    if request.method == 'POST':
+        if 'yes' in request.POST.keys():
+            chore_id = request.POST['yes']
+            chore = Chore.get_chore(chore_id)
+            Vote.create_new_vote(request.user, chore, True)
+        else:
+            chore_id = request.POST['no']
+            chore = Chore.get_chore(chore_id)
+            Vote.create_new_vote(request.user, chore, False)
+        return redirect('commune')
+    else:
+        return redirect('commune')
 
 
 def user_logout(request):
