6b829a794e87c2f5c4761a7253c020db576e24e4
RonTuretzkyMigal <turetzkyron@gmail.com>
2023-01-21 15:04:08 +0200

    adding vote function, updating commune view to include more details

--- commune_app/views.py
+++ commune_app/views.py
@@ -2,11 +2,24 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import get_user_model
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from commune_app.models import Chore, Commune
+from commune_app.models import Chore, Commune, Vote
 
 
 def main_page(request):
     return render(request, 'commune_app/index.html')
+
+
+def get_yes_votes_for_chore(chore_id):
+    return len(Vote.objects.filter(chore_id=chore_id, approve=True))
+
+
+def get_no_votes_for_chore(chore_id):
+    return len(Vote.objects.filter(chore_id=chore_id, approve=False
+                                   ))
+
+
+def has_voted(user_id, chore_id):
+    return len(Vote.objects.filter(user_id=user_id, chore_id=chore_id)) > 0
 
 
 def commune(request):
@@ -14,6 +27,8 @@ def commune(request):
     # chores = Chore.objects.filter(commune_id=commune_id, passed=False)
     active_chores = Chore.objects.filter(commune_id=commune_id, completed=False, passed=True)
     chores_to_vote_on = Chore.objects.filter(commune_id=commune_id, completed=False, passed=False)
+    chores_to_vote_on = [(chore, get_yes_votes_for_chore(chore.id), get_no_votes_for_chore(chore.id),
+                          has_voted(request.user.id, chore.id), chore.id) for chore in chores_to_vote_on]
     commune = Commune.objects.filter(id=commune_id).first()
     commune_name = commune.name
     wallet = commune.wallet
@@ -57,6 +72,21 @@ def user_login(request):
             return redirect('login')
     else:
         return render(request, 'commune_app/login.html')
+
+
+def vote(request):
+    if request.method == 'POST':
+        if 'yes' in request.POST.keys():
+            chore_id = request.POST['yes']
+            chore = Chore.get_chore(chore_id)
+            Vote.create_new_vote(request.user, chore, True)
+        else:
+            chore_id = request.POST['no']
+            chore = Chore.get_chore(chore_id)
+            Vote.create_new_vote(request.user, chore, False)
+        return redirect('commune')
+    else:
+        return redirect('commune')
 
 
 def user_logout(request):
